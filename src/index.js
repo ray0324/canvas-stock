58b444570cd832000b2dddbdd95dcf74ae9db9f0
ray0324 <ray0324@foxmail.com>
2017-06-29 12:44:42 +0800

    add mock data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
-function int() {
-    console.log('hello');
+
+const canvas = document.querySelector('canvas');
+const dpr = 2 || window.devicePixelRatio || 1;
+const width = 800;
+const height= 600;
+
+canvas.style.width=`${width}px`;
+canvas.style.height=`${height}px`;
+canvas.style.border=`1px solid #ccc`;
+
+
+canvas.width = width*dpr;
+canvas.height= height*dpr;
+const ctx =  canvas.getContext('2d');
+
+class Grid {
+    constructor(ctx){
+        ctx.scale(dpr,dpr);
+        ctx.translate(0.5,0.5);
+        ctx.strokeStyle = '#ddd';
+        ctx.strokeWidth = 1;
+        this.ctx =  ctx;
+    }
+    render(){
+        const ctx = this.ctx;
+        for(let y = 30,step = 30; y <= height; y += step){
+            ctx.moveTo(60 ,y);
+            ctx.lineTo(width-60 ,y);
+        }
+        for(let x = 60,step = 200; x <= width; x += step){
+            ctx.moveTo(x,30 );
+            ctx.lineTo(x ,height-30);
+        }
+        ctx.stroke();
+        ctx.save();
+        console.log(ctx);
+    }
 }
+
+const grid = new Grid(ctx);
+console.log(grid.render());
